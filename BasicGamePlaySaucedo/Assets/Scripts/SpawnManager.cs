96ab47ee8c7d18e69ff4977656a5dcf7ae3e280a
MathSauc3 <732584@student.dusd.net>
2023-03-10 11:40:44 -0800

    nothing much

--- BasicGamePlaySaucedo/Assets/Scripts/SpawnManager.cs
+++ BasicGamePlaySaucedo/Assets/Scripts/SpawnManager.cs
@@ -13,6 +13,11 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
+                animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
