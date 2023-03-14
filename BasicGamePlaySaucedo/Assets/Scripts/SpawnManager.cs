7f21c7674025ab086182dac75d61250decf267a3
MathSauc3 <732584@student.dusd.net>
2023-03-14 11:40:29 -0700

    finish 2.3

--- BasicGamePlaySaucedo/Assets/Scripts/SpawnManager.cs
+++ BasicGamePlaySaucedo/Assets/Scripts/SpawnManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
     public GameObject[] animalPrefabs;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,9 +17,13 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
+
+               Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
+                0, spawnPosZ);
             int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
+            Instantiate(animalPrefabs[animalIndex], spawnPos,
                 animalPrefabs[animalIndex].transform.rotation);
         }
+        
     }
 }
